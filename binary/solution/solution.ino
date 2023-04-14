14bf9e87bd12971c14f94cd38c7adcc40df25669
Jaroslav Kusák <jarekusak@seznam.cz>
2023-04-14 22:25:21 +0200

    znovupřidání binární čítačky

--- binary/solution/solution.ino
+++ binary/solution/solution.ino
@@ -1,4 +1,4 @@
-#include <funshield.h>
+//#include <funshield.h>
 
 constexpr int leds[] = {led1_pin, led2_pin, led3_pin, led4_pin};
 constexpr int led_counter = sizeof(leds) / sizeof(leds[0]); // number of leds
@@ -7,7 +7,7 @@ constexpr int DELAY = 300;
 constexpr int ONE_SECOND = 1000;
 constexpr int MAX_NUMBER = 16;
 int binary_number[led_counter]; // array, which saves given number as ones and zeros
-int number_to_show = 0;
+int numberToShow = 0;
 unsigned long lastTime;
 
 class Button {
@@ -16,8 +16,8 @@ public:
   byte Pressed;
   byte Was_pressed;
   bool Inc_or_dec; // true = increment, false = decrement
-  bool One_second_passed;
-  int One_second_timer;
+  bool one_second_passed;
+  int one_second_timer;
   int timer;
 
   Button(int button_pin, byte is_pressed, byte was_pressed, bool inc_or_dec) {
@@ -25,8 +25,8 @@ public:
     Pressed = is_pressed;
     Was_pressed = was_pressed; 
     Inc_or_dec = inc_or_dec;
-    One_second_passed = false;
-    One_second_timer = 0;
+    one_second_passed = false;
+    one_second_timer = 0;
     timer=0;
   }
 
@@ -43,11 +43,11 @@ void toBinary(int n){
 }
       
 void displayBinary(bool inc_or_dec) {
-  number_to_show = (inc_or_dec) ? number_to_show+1 : number_to_show-1;
-  if (number_to_show < 0)
-    number_to_show = MAX_NUMBER-1; // set as MAX_NUMBER-1 (only ones in binary)
-  number_to_show=number_to_show%MAX_NUMBER; // modulo 16
-  toBinary(number_to_show); // transfers number to binary
+  numberToShow = (inc_or_dec) ? numberToShow+1 : numberToShow-1;
+  if (numberToShow < 0)
+    numberToShow = MAX_NUMBER-1; 
+  numberToShow=numberToShow%MAX_NUMBER; // modulo 16
+  toBinary(numberToShow); // transfers number to binary
   for (int i = 0; i < led_counter; i++){
     if(binary_number[i] == 1)
       digitalWrite(leds[i], ON);
@@ -71,29 +71,29 @@ void setup() {
 
 void resetValues(Button &button){ // pass by reference
   button.Was_pressed = OFF; // sets last button state as non-holded
-  button.One_second_timer = 0; // resets one second timer
-  button.One_second_passed = false; // sets as one second hasn't passed
+  button.one_second_timer = 0; // resets one second timer
+  button.one_second_passed = false; // sets as one second hasn't passed
   button.timer = 0; 
 }
 
 void wasButtonPressed(Button &button, long deltaTime){
-  byte button_state = digitalRead(button.pin);
-  button.Pressed = button_state;
+  byte buttonState = digitalRead(button.pin);
+  button.Pressed = buttonState;
   if (button.Pressed == ON){
     if (button.Was_pressed == OFF) // if button wasn't holded in previous iteration
       displayBinary(button.Inc_or_dec); 
      
     button.Was_pressed = ON; // sets last button's state as holded
-    button.One_second_timer+=deltaTime; // one second begins to be counted
+    button.one_second_timer+=deltaTime; // one second begins to be counted
     
-    if (button.One_second_passed) // if one second passed after still holding the button, then start counting the second timer
+    if (button.one_second_passed) // if one second passed after still holding the button, then start counting the second timer
       button.timer+=deltaTime;
-    if (button.timer >= DELAY && button.One_second_passed){ // starts displaying binary numbers after holding the button for 1 second
+    if (button.timer >= DELAY && button.one_second_passed){ // starts displaying binary numbers after holding the button for 1 second
       displayBinary(button.Inc_or_dec);
       button.timer-=DELAY;
     }
-    if (button.One_second_timer >= ONE_SECOND && !button.One_second_passed){  // if one second passed and One_second_passed hasn't been set
-        button.One_second_passed = true;   
+    if (button.one_second_timer >= ONE_SECOND && !button.one_second_passed){  // if one second passed and one_second_passed hasn't been set
+        button.one_second_passed = true;   
         displayBinary(button.Inc_or_dec);
     }
   }
