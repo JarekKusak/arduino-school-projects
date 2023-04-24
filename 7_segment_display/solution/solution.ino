890996334cdf54f0ec00082352bd6ba937443c1f
Jaroslav Kusák <jarekusak@seznam.cz>
2023-04-24 19:58:49 +0200

    pár změn v modifikátorech přístupnosti

--- 7_segment_display/solution/solution.ino
+++ 7_segment_display/solution/solution.ino
@@ -9,40 +9,12 @@ constexpr int orderCount = sizeof(orderToShow) / sizeof(orderToShow[0]);
 int order = orderCount-1; // sets as first order (otherway around)
 constexpr int MAX_NUMBER = 10000; // upper barrier
 
-class Button {
-  public:
-    int Pin;
-    bool IncOrDec; // true = increment, false = decrement/nothing
-    bool Switch_button;
-    byte Pressed;
-    byte WasPressed;
-    
-  Button(int button_pin, bool inc_or_dec, bool does_switch) {
-    Pin = button_pin;
-    IncOrDec = inc_or_dec;
-    Switch_button = does_switch;
-    Pressed = OFF;
-    WasPressed = OFF;
-  }
-
-  void setupPin() {
-    pinMode(Pin, INPUT);
-  }
-};
-
-Button button1(button1_pin, true, false);
-Button button2(button2_pin, false, false);
-Button button3(button3_pin, false, true); // button that switches into different power of ten
-Button buttons[3] = {button1, button2, button3};
-constexpr int buttonsCount = sizeof(buttons) / sizeof(buttons[0]);
-
 int returnDigit(int number) {
   int i = 0;
   int remainder;
 
-  // if the showed order is higher than actul number, then just return 0 as digit to show
-  for (int j = 0; j < orderCount; j++)
-  {
+  // if the showed order is higher than actual number, then just return 0 as digit to show
+  for (int j = 0; j < orderCount; j++) {
       if (number < pow(10,j+1) && orderCount-order-1 > j)
           return 0;
   }
@@ -59,11 +31,68 @@ int returnDigit(int number) {
 }
 
 void displayOutput(int order, int digit) {
-  shiftOut(data_pin, clock_pin, MSBFIRST, digits[digit]);   // Display digit 7 (11111000) ...
-  shiftOut(data_pin, clock_pin, MSBFIRST, order);     // ... on positions 1 and 3 (0101)
-  digitalWrite(latch_pin, LOW);                   // Trigger the latch
+  shiftOut(data_pin, clock_pin, MSBFIRST, digits[digit]); // Display digit 7 (11111000) ...
+  shiftOut(data_pin, clock_pin, MSBFIRST, order); // ... on positions 1 and 3 (0101)
+  digitalWrite(latch_pin, LOW); // Trigger the latch
   digitalWrite(latch_pin, HIGH);
 }
+
+class Button {
+  private:
+    int pin;
+    bool incOrDec; // true = increment, false = decrement/nothing
+    bool switchButton;
+    byte pressed;
+    byte wasPressed;
+  public:
+  Button(int button_pin, bool inc_or_dec, bool does_switch) {
+    pin = button_pin;
+    incOrDec = inc_or_dec;
+    switchButton = does_switch;
+    pressed = OFF;
+    wasPressed = OFF;
+  }
+
+  void setupPin() {
+    pinMode(pin, INPUT);
+  }
+
+  void action() {
+    if (switchButton) { // button that switches order to show
+      order--; // smaller value of order = higher actual order of number
+      if (order < 0) 
+        order=orderCount-1; // underflow (order overflow) - just sets as the first order
+    }
+    else if (incOrDec) { // true == increment
+      number=number+pow(10,orderCount-order-1); // add's current order's power of ten
+      number=number%MAX_NUMBER;
+    }
+    else { // decrement
+      number=number-pow(10,orderCount-order-1); // substract's current order's power of ten
+      if (number < 0) // if underflow, substract max number and the negative value
+        number=MAX_NUMBER+number;
+    }
+    displayOutput(orderToShow[order], returnDigit(number)); // outputs on display, two parameters: order and the digit of number
+  }
+
+  void wasButtonPressed() {
+    byte buttonState = digitalRead(pin);
+    pressed = buttonState;
+    if (pressed == ON) {
+      if (wasPressed == OFF) // if button wasn't holded in previous iteration
+        action();
+      wasPressed = ON; // sets last button's state as held
+    }
+    else
+       wasPressed = OFF;
+  }
+};
+
+Button button1(button1_pin, true, false);
+Button button2(button2_pin, false, false);
+Button button3(button3_pin, false, true); // button that switches into different power of ten
+Button buttons[3] = {button1, button2, button3};
+constexpr int buttonsCount = sizeof(buttons) / sizeof(buttons[0]);
 
 void setup() {
   for (int i = 0; i < buttonsCount; i++)
@@ -73,38 +102,8 @@ void setup() {
   pinMode(data_pin, OUTPUT);
   displayOutput(orderToShow[order], number); // displays zero
 }
-  
-void action(Button& button) {
-  if (button.Switch_button) { // button that switches order to show
-    order--; // smaller value of order = higher actual order of number
-    if (order < 0) 
-      order=orderCount-1; // underflow (order overflow) - just sets as the first order
-  }
-  else if (button.IncOrDec) { // true == increment
-    number=number+pow(10,orderCount-order-1); // add's current order's power of ten
-    number=number%MAX_NUMBER;
-  }
-  else { // decrement
-    number=number-pow(10,orderCount-order-1); // substract's current order's power of ten
-    if (number < 0) // if underflow, substract max number and the negative value
-      number=MAX_NUMBER+number;
-  }
-  displayOutput(orderToShow[order], returnDigit(number)); // outputs on display, two parameters: order and the digit of number
-}
-
-void wasButtonPressed(Button& button) {
-  byte buttonState = digitalRead(button.Pin);
-  button.Pressed = buttonState;
-  if (button.Pressed == ON) {
-    if (button.WasPressed == OFF) // if button wasn't holded in previous iteration
-      action(button);
-    button.WasPressed = ON; // sets last button's state as held
-  }
-  else
-     button.WasPressed = OFF;
-}
 
 void loop() {
   for (int i = 0; i < buttonsCount; i++)
-    wasButtonPressed(buttons[i]); 
+    buttons[i].wasButtonPressed(); 
 }
