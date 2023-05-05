578b9aa629e071971ff45539e9452896146a9807
Jaroslav Kusák <jarekusak@seznam.cz>
2023-05-05 17:20:27 +0200

    lehké zdokumentování kódu

--- dun_and_drag/kusak/kusak.ino
+++ dun_and_drag/kusak/kusak.ino
@@ -1,6 +1,5 @@
 #include "funshield.h"
 
-unsigned long timer = 0;
 unsigned long lastTime;
 bool previousMode = true; // mode before touching random button: true = normal, false = conf.
 bool mode = true; // mode after pushing down the button: true = normal, false = conf.
@@ -24,7 +23,7 @@ class Button {
   void setupPin() {
     pinMode(pin, INPUT);
   }
-  // can detect longer holding the button
+  // can detect a longer button hold
   bool isButtonBeingHeld() {
     bool buttonState = digitalRead(pin);
     return buttonState;
@@ -51,18 +50,12 @@ class Display {
     const int tens = 4;
     const int hundreds = 2;
     const int thousands = 1;
-    int index;
+    int index = 0;
     byte d = 0b10100001; // letter d 
     int orderCount = sizeof(digit_muxpos) / sizeof(digit_muxpos[0]);
   public:
   
   void displayOutput(int order, int digit, bool mode) { 
-    /*
-    if (digit == 0 && order == hundreds && timer < 10000)
-      return;
-    if (digit == 0 && order == thousands && timer < 100000)
-      return;
-    */
     shiftOut(data_pin, clock_pin, MSBFIRST, (order == hundreds && !mode) ? digit : digits[digit]); // if order is at ones, show decimal point
     shiftOut(data_pin, clock_pin, MSBFIRST, order); // ... on positions 1 and 3 (0101)
     digitalWrite(latch_pin, LOW); // Trigger the latch
@@ -74,12 +67,12 @@ class Display {
     return (number / multiplier) % 10;
   }
 
-  void incrementIndex(){
+  void incrementIndex() {
     index++;
     index%=orderCount;
   }
 
-  void showOnDisplay(int n, bool mode){
+  void showOnDisplay(int n, bool mode) {
     displayOutput(digit_muxpos[orderCount-index-1], getDigitAtPosition(n, index), mode); // getDigitAtPosition(n, index)
     incrementIndex();
   }
@@ -90,7 +83,7 @@ class Display {
   }
   
   void displayConfigurationMode(int numberOfThrows, int diceType, bool mode) {
-    if (digit_muxpos[orderCount-index-1] == hundreds) // show staticly letter d on hundreds order
+    if (digit_muxpos[orderCount-index-1] == hundreds) // show staticly letter "d" on hundreds order
       showOnDisplayOneNumber(d, mode);
     else if (digit_muxpos[orderCount-index-1] == thousands) // show staticly number of throws on thousand's place
       showOnDisplayOneNumber(numberOfThrows, mode); 
@@ -119,14 +112,14 @@ class Dice {
   int returnDiceType() { // getter for type of dice
     return dices[diceType];
   }
-  int returnNumberOfThrows(){ // getter for number of throws
+  int returnNumberOfThrows() { // getter for number of throws
     return numberOfThrows;
   }
-  int returnGeneratedRandomNumber(){
+  int returnGeneratedRandomNumber() { // getter for randomNumber
     return randomNumber;
   }
   
-  void increaseTimer(long deltaTime){
+  void increaseTimer(long deltaTime) {
     generatingSeed = true;
     timer += deltaTime;
   }
@@ -134,9 +127,7 @@ class Dice {
   int checkIfSeedGenerated() {
     if (!generatingSeed && timer > 0) { // if generating seed already stopped and timer is non-zero, then generate random number
       randomSeed(timer);
-      randomNumber = numberOfThrows*random(1,dices[diceType]); // 1 to 4 chance
-      Serial.println(timer);
-      Serial.println(randomNumber);
+      randomNumber = numberOfThrows*random(1,dices[diceType]);
       timer = 0;
     }
     generatingSeed = false;
@@ -147,19 +138,17 @@ class Dice {
     numberOfThrows+=value;
     if (numberOfThrows == maxNumberOfThrows)
       numberOfThrows = 1;
-    Serial.println(numberOfThrows);
   }
 
   void changeDiceType(){
     diceType++;
     diceType%=dicesCount;
-    Serial.println(dices[diceType]);
   }
 
 } dice;
 
 Button button1(button1_pin); // normal mode (generates random seed)
-Button button2(button2_pin, 1); // conf. mode (increases number of throws)
+Button button2(button2_pin, 1); // conf. mode (increases number of throws - by one)
 Button button3(button3_pin); // conf. mode (increases sides of dice)
 Button buttons[3] = {button1, button2, button3};
 constexpr int buttonsCount = sizeof(buttons) / sizeof(buttons[0]);
@@ -168,47 +157,39 @@ void setup() {
   lastTime = millis(); // time at start
   for (int i = 0; i < buttonsCount; i++)
     buttons[i].setupPin();
-  randomSeed(analogRead(0)); // inicialize randomSeed
+  randomSeed(analogRead(0)); // initialize randomSeed
   pinMode(latch_pin, OUTPUT);
   pinMode(clock_pin, OUTPUT);
   pinMode(data_pin, OUTPUT);
-  Serial.begin(9600);
 }
 
-constexpr int DELAY = 200;
 void loop() {
   unsigned long currentTime = millis(); // Time since start
   unsigned long deltaTime = currentTime - lastTime; // Time since last loop
   bool seedGeneratingButtonBeingHeld = (buttons[0].isButtonBeingHeld() == ON) ? true : false;
   if (seedGeneratingButtonBeingHeld) {
-    mode = true;
-    if (previousMode)
+    mode = true; // switches mode to normal mode (true)
+    if (previousMode) // if previous mode was configuration mode, then don't do anything until user unholds the button (we want only to switch into normal mode, not immediately generate)
       dice.increaseTimer(deltaTime);      
   }
   else if (buttons[1].wasButtonPressed()) { // button that increments number of throws (conf. mode)
-    mode = false;
-    //(previousMode == mode) ? dice.increaseNumberOfThrows() : previousMode = mode; 
-    if (previousMode == mode) {
+    mode = false; // switches mode to configuration mode (false)
+    //(previousMode == mode) ? dice.increaseNumberOfThrows(buttons[1].buttonValue()) : previousMode = mode; // cannot be written as ternary operator ???
+    if (previousMode == mode) 
       dice.increaseNumberOfThrows(buttons[1].buttonValue());
-      Serial.println("přičítáme počet hodů");
-    } 
-    else {
+    else 
       previousMode = mode;
-      Serial.println("mění se mode v 2");
-    }
   }
   else if (buttons[2].wasButtonPressed()) { // button that switches orders (conf. mode)
-    mode = false;
-    //(previousMode == mode) ? dice.changeDiceType() : (previousMode = mode);
-    if (previousMode == mode) {
-      dice.changeDiceType();
-    } 
-    else {
-      previousMode = mode;
-    }
+    mode = false; // switches mode to configuration mode (false)
+    //(previousMode == mode) ? dice.changeDiceType() : (previousMode = mode); // cannot be written as ternary operator ???
+    if (previousMode == mode) 
+      dice.changeDiceType();   
+    else 
+      previousMode = mode;    
   }
-  if (previousMode != mode && !seedGeneratingButtonBeingHeld) // if previous mode isn't actual and seed generating button is not being held anymore, then set present mode
+  if (previousMode != mode && !seedGeneratingButtonBeingHeld) // if previous mode isn't actual and seed generating button is not being held anymore, then set current mode
     previousMode = mode;
-  (mode) ? display.displayNormalMode(dice.checkIfSeedGenerated(), mode) : display.displayConfigurationMode(dice.returnNumberOfThrows(), dice.returnDiceType(), mode);
+  (mode) ? display.displayNormalMode(dice.checkIfSeedGenerated(), mode) : display.displayConfigurationMode(dice.returnNumberOfThrows(), dice.returnDiceType(), mode); // display depends on current mode
   lastTime = currentTime;
 }
