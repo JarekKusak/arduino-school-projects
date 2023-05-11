a64fe8daf8c82c31e2779f65127414e7b9b586aa
Jaroslav Kusák <jarekusak@seznam.cz>
2023-05-11 09:31:48 +0200

    finální konečná verze

--- text/solution/solution.ino
+++ text/solution/solution.ino
@@ -2,57 +2,53 @@
 #include "input.h"
 unsigned long lastTime;
 // map of letter glyphs
-constexpr byte LETTER_GLYPH[] {
-  0b10001000,   // A
-  0b10000011,   // b
-  0b11000110,   // C
-  0b10100001,   // d
-  0b10000110,   // E
-  0b10001110,   // F
-  0b10000010,   // G
-  0b10001001,   // H
-  0b11111001,   // I
-  0b11100001,   // J
-  0b10000101,   // K
-  0b11000111,   // L
-  0b11001000,   // M
-  0b10101011,   // n
-  0b10100011,   // o
-  0b10001100,   // P
-  0b10011000,   // q
-  0b10101111,   // r
-  0b10010010,   // S
-  0b10000111,   // t
-  0b11000001,   // U
-  0b11100011,   // v
-  0b10000001,   // W
-  0b10110110,   // ksi
-  0b10010001,   // Y
-  0b10100100,   // Z
+constexpr byte LETTER_GLYPH[]{
+  0b10001000,  // A
+  0b10000011,  // b
+  0b11000110,  // C
+  0b10100001,  // d
+  0b10000110,  // E
+  0b10001110,  // F
+  0b10000010,  // G
+  0b10001001,  // H
+  0b11111001,  // I
+  0b11100001,  // J
+  0b10000101,  // K
+  0b11000111,  // L
+  0b11001000,  // M
+  0b10101011,  // n
+  0b10100011,  // o
+  0b10001100,  // P
+  0b10011000,  // q
+  0b10101111,  // r
+  0b10010010,  // S
+  0b10000111,  // t
+  0b11000001,  // U
+  0b11100011,  // v
+  0b10000001,  // W
+  0b10110110,  // ksi
+  0b10010001,  // Y
+  0b10100100,  // Z
 };
 constexpr byte EMPTY_GLYPH = 0b11111111;
+SerialInputHandler input;
 
 class Display {
-  private:
-    const int positionsCount = 4;
-    const unsigned int scrollingInterval = 300;
-    const int orderCount = sizeof(digit_muxpos) / sizeof(digit_muxpos[0]);
-    char* message[4] = {" "," "," "," "};
-    unsigned long lastScrollTime = 0;
-    int displayIndex = 3;
-    int messageIndex = 0;
-    int timer = 0;
-    int space[1] = {" "};
 
-  public:
-    void setupDisplay() {
-      pinMode(latch_pin, OUTPUT);
-      pinMode(clock_pin, OUTPUT);
-      pinMode(data_pin, OUTPUT);
-    }
+private:
+  const int orderCount = sizeof(digit_muxpos) / sizeof(digit_muxpos[0]);
+  int timer = 0;
+  int scrollingInterval = 300;
+  int messageLength = 0;
+  int messageIndex = -orderCount;
+  int position = 3;
+  const char* message;
+  bool messageReceived = false;
 
-    void displayChar(char ch, byte pos) {
-      byte glyph = EMPTY_GLYPH;
+public:
+ 
+  void displayChar(char ch, byte pos) {
+    byte glyph = EMPTY_GLYPH;
       if (isAlpha(ch)) {
         glyph = LETTER_GLYPH[ ch - (isUpperCase(ch) ? 'A' : 'a') ];
       }
@@ -61,41 +57,54 @@ class Display {
       shiftOut(data_pin, clock_pin, MSBFIRST, glyph);
       shiftOut(data_pin, clock_pin, MSBFIRST, pos);
       digitalWrite(latch_pin, HIGH);
-    }
+  }
 
-    void displayString(const char* text, long deltaTime) {
-      timer+=deltaTime;
-      // Update message
-      if (timer >= scrollingInterval) {
-        timer=0;
-        messageIndex++;
-        if (text[messageIndex] == '\0') 
-          messageIndex = 0;
-        for (int i = 0; i < positionsCount; i++) 
-          message[i] = text[messageIndex + i]; 
+  int length(const char* string) {
+    int i = 0;
+    while (string[i])
+      i++;
+    return i;
+  }
+  
+  void displayMessage(long deltaTime) {
+    timer+=deltaTime;
+    if (timer >= scrollingInterval) {
+      timer -= scrollingInterval;
+      messageIndex++;
+      if (messageIndex >= messageLength) {
+        messageIndex = -orderCount;
+        messageReceived = false;
       }
-      
-      // Display message
-      displayChar(message[displayIndex], digit_muxpos[displayIndex]);
-      displayIndex--;
-      if (displayIndex < 0) 
-        displayIndex = 3;     
     }
-};
+    displayChar((messageIndex + position < 0) || (messageIndex + position > messageLength) ? EMPTY_GLYPH : message[messageIndex + position], digit_muxpos[position]);
+    position--;
+    if (position < 0) 
+      position = orderCount-1;
+  }
 
-SerialInputHandler input;
-
-Display display;
+  bool checkIfMessageReceived() {
+    if (!messageReceived) { 
+      message = input.getMessage();
+      messageLength = length(message);
+      messageReceived = true;
+    }
+    return messageReceived;
+  }
+}display;
 
 void setup() {
-  display.setupDisplay();
+  pinMode(latch_pin, OUTPUT);
+  pinMode(clock_pin, OUTPUT);
+  pinMode(data_pin, OUTPUT);
+
   input.initialize();
-}
+};
 
 void loop() {
   unsigned long currentTime = millis();
   unsigned long deltaTime = currentTime - lastTime;
-  display.displayString(input.getMessage(), deltaTime);
   input.updateInLoop();
+  if (display.checkIfMessageReceived())
+    display.displayMessage(deltaTime);
   lastTime = currentTime;
 }
